f9a39d492eb2e86d2932064df33656a4677ec723
jacket <josh@bkmediagroup.com>
2014-05-20 15:29:12 -0600

    new version

---
+++ config/initializers/blogasaurus_assets.rb
@@ -0,0 +1,6 @@
+Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/javascripts/fileuploader.js
+                                                  ckeditor/filebrowser/javascripts/jquery.endless-scroll.js 
+ckeditor/filebrowser/javascripts/jquery.min.js 
+ckeditor/filebrowser/javascripts/jquery.tmpl.min.js
+ckeditor/filebrowser/javascripts/rails.js
+ckeditor/filebrowser/stylesheets/uploader.css)
